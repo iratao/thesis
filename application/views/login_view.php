8ad869a39e6c9800ad4be7431c5d3334e39ced17
Ira <iratao@gmail.com>
2014-04-06 15:22:27 +0200

    login ui polish

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,14 +1,24 @@
-<div class="container-fluid">
+<div class="container">
     <h1>Simple Login with CodeIgniter</h1>
     <?php echo validation_errors(); ?>
-    <?php echo form_open('verifylogin'); ?>
-        <label for="username">Username:</label>
-        <input type="text" size="20" id="username" name="username"/>
-        <br/>
-        <label for="password">Password:</label>
-        <input type="password" size="20" id="passowrd" name="password"/>
-        <br/>
-        <input type="submit" value="Login"/>
+    <form class="form-horizontal" role="form" action="<?php echo site_url('verifylogin')?>" method="post">
+        <div class="form-group">
+            <label for="username" class="col-sm-2 control-label">Username:</label>
+            <div class="col-sm-4">
+                <input type="text" id="username" name="username" class="form-control"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="password" class="col-sm-2 control-label">Password:</label>
+            <div class="col-sm-4">
+                <input type="password" id="passowrd" name="password" class="form-control"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-default">Sign in</button>
+            </div>
+        </div>
     </form>
 </div>
 
