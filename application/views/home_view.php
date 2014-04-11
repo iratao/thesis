35f1cecd8007f54d3b1c23c1cb0315489a5fbebe
Ira <iratao@gmail.com>
2014-04-11 21:06:49 +0200

    add statements

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -9,6 +9,17 @@
 <div class="container">
     <div style="margin-bottom: 50px;">
         <h1>Hello <?php echo $username?></h1>
+        <p>
+            I would like to invite you to this survey. Through this survey, we 
+            would like collect data for the study of the influence of context 
+            factors to users' offline shopping behavior. <br/>
+            In this survey, all the information will be anonymously recorded.          
+            If you are fine with all statements above. Please go ahead.
+        </p>
+        <p>
+            If you are a woman, please click button "survey(women)". If you are a 
+            men, please click button "survey(men)". <br/>
+        </p>
     </div>
     
     
